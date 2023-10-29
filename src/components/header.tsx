219458e2c41609e30a18c84128ac3dd47e305cb4
G-Merich <gmerit2030@gmail.com>
2023-10-29 22:15:50 +0000

    clean editor

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export conts header = () => {
-  return <div>
+  return (
+    <div className="bg-primary text-light py-4 text-center">
     <h1>CleanEditor</h1>
-
-  </div>
- 
-}
+    </div>
+ );
+};
